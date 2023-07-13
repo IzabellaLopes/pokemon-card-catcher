daf540ba00f0bf0e1f8e81306b6cdf89a86a0ee0
IzabellaLopes <>
2023-07-13 15:31:51 +0000

    Add basic structure of level of difficulty modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,5 +18,22 @@ function handleSubmit(event) {
     event.preventDefault();
     
     localStorage.setItem('player', input.value);
-    window.location = 'game.html'
 }
+
+
+//Level of difficulty
+//Created using the tutorial - https://www.youtube.com/watch?v=XH5OW46yO8I
+const login_button = document.getElementById('login_button');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+const btn_easy = document.getElementById('btn_easy');
+const btn_medium = document.getElementById('btn_medium');
+const btn_hard = document.getElementById('btn_hard');
+
+login_button.addEventListener('click', () => {
+    modal_container.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+});
